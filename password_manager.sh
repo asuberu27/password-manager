a5a472585cee6ed975012ce20bf8a491e3cf91e5
asuberu_27 <tsudakazumasa1027@gmail.com>
2024-12-15 11:22:04 +0900

    コードのリファクタリングを行いました

--- password_manager.sh
+++ password_manager.sh
@@ -3,14 +3,20 @@
 filename=database.txt                   # パスワード保存先のファイル名を決める
 COMMON_KEY=$(printenv QUEST_PASSPHRASE) # 暗号化・復号化するためのパスフレーズを環境変数から読み込む
 
-# 同名のサービスがないかをチェックする関数
-function is_sameservice() {
-    grep -q "^${1}:" "${filename}" 2>/dev/null
+function decrypt_file {
+    gpg \
+        --passphrase="${COMMON_KEY}" \
+        --batch \
+        --yes \
+        --output "${filename}" \
+        --yes \
+        --decrypt \
+        "${filename}.gpg" 2>/dev/null
 }
 
-# 文字列にコロンが含まれているかをチェックする関数
-function is_colon() {
-    echo "${1}" | grep -q ":" 2>/dev/null
+function encrypt_file {
+    echo "${COMMON_KEY}" | gpg --passphrase-fd 0 --batch --yes --symmetric --cipher-algo AES256 "${filename}" 2>/dev/null
+    rm "${filename}" 2>/dev/null
 }
 
 echo "パスワードマネージャーへようこそ！"
@@ -24,65 +30,61 @@ while [ true ]; do
         echo
 
         # ファイルの復号化
-        gpg \
-            --passphrase="${COMMON_KEY}" \
-            --batch \
-            --yes \
-            --output "${filename}" \
-            --yes \
-            --decrypt \
-            "${filename}.gpg" 2>/dev/null
+        decrypt_file
 
         # 入力のバリデーション
-        is_sameservice "${servicename}"
+        grep -q "^${servicename}:" "${filename}" 2>/dev/null
         if [[ $? == 0 ]]; then
             echo "既に同名のサービス名が登録されています。サービス名入力からやり直して下さい。"
             continue
         fi
-        is_colon "${servicename}${username}${password}"
+        echo "${servicename}${username}${password}" | grep -q ":" 2>/dev/null
         if [[ $? == "0" ]]; then
-            echo "登録にコロンは使用できません。サービス名入力からやり直して下さい。"
+            echo "登録文字に半角のコロンは使用できません。サービス名入力からやり直して下さい。"
+            encrypt_file
             continue
         fi
-        # 追記＆暗号化
-        echo "${servicename}:${username}:${password}" >>"${filename}" &&
-            echo "${COMMON_KEY}" | gpg --passphrase-fd 0 --batch --yes --symmetric --cipher-algo AES256 "${filename}" &&
-            rm "${filename}" &&
-            echo "パスワードの追加は成功しました。"
+        echo "${servicename}${username}${password}" | grep -q "" 2>/dev/null
+        if [[ "${servicename}" == "" || "${username}" == "" || "${password}" == "" ]]; then
+            echo "1文字以上入力してください。サービス名入力からやり直して下さい。"
+            encrypt_file
+            continue
+        fi
+
+        # ファイルに追記＆暗号化
+        echo "${servicename}:${username}:${password}" >>"${filename}"
+        encrypt_file
+        echo "パスワードの追加は成功しました。"
+
         read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" selection
     elif [[ ${selection} == "Get Password" ]]; then
         read -p "サービス名を入力してください：" servicename
 
         # ファイルの復号化
-        gpg \
-            --passphrase="${COMMON_KEY}" \
-            --batch \
-            --yes \
-            --output "${filename}" \
-            --yes \
-            --decrypt \
-            "${filename}.gpg" 2>/dev/null
+        decrypt_file
+
+        #復号化に成功した場合、サービス名が保存されているかを確認
         if [[ $? == "0" ]]; then
-            info=$(grep "^${servicename}:" "${filename}")
+            is_registered=$(grep "^${servicename}:" "${filename}")
         fi
-        if [[ ${info} ]]; then
-            servicename=${info%%:*}
-            tmp=${info#*:}
+        if [[ ${is_registered} ]]; then
+            servicename=${is_registered%%:*}
+            tmp=${is_registered#*:}
             username=${tmp%%:*}
-            password=${info##*:}
+            password=${is_registered##*:}
             echo
             echo "サービス名：${servicename}"
             echo "ユーザー名：${username}"
             echo "パスワード：${password}"
-            # 暗号化
-            echo "${COMMON_KEY}" | gpg --passphrase-fd 0 --batch --yes --symmetric --cipher-algo AES256 "${filename}" &&
-                rm "${filename}"
         else
             echo "そのサービスは登録されていません。"
         fi
+
+        # ファイルの暗号化
+        encrypt_file
+
         echo
         read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" selection
-
     elif [[ ${selection} == "Exit" ]]; then
         echo "Thank you!"
         break
